c37b1d6b191899ad446a40c2ba7aa9ac1a37f5ed
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-26 18:40:55 +0530

    initial commit

---
+++ src/main/java/com/cityzen/lockermanagementservice/LockermanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cityzen.lockermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockermanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockermanagementserviceApplication.class, args);
+	}
+
+}
