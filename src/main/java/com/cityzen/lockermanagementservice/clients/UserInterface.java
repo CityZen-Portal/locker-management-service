f3ccf92f737d6fd98ac7122cd50d0a5537c0cca8
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-30 08:13:55 +0530

    feat:added deployed link on interface

--- src/main/java/com/cityzen/lockermanagementservice/clients/UserInterface.java
+++ src/main/java/com/cityzen/lockermanagementservice/clients/UserInterface.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@FeignClient(name = "UserManagementService", url = "${user.service.url}")
+@FeignClient(name = "UserManagementService", url = "https://auth-backend-cpcr.onrender.com")
 public interface UserInterface {
 
     @GetMapping("/api/auth/userInfo/{aadharNumber}")
--- src/main/java/com/cityzen/lockermanagementservice/controller/LockerController.java
+++ src/main/java/com/cityzen/lockermanagementservice/controller/LockerController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/api/lock")
 public class LockerController {
