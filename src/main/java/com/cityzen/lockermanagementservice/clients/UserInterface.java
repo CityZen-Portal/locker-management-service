6b10816c6336e529b25011905658dabe456e7aaa
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-08-06 22:27:58 +0530

    feat : commented validate token lines

--- src/main/java/com/cityzen/lockermanagementservice/clients/UserInterface.java
+++ src/main/java/com/cityzen/lockermanagementservice/clients/UserInterface.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@FeignClient(name = "UserManagementService", url = "https://auth-backend-obcu.onrender.com")
+@FeignClient(name = "UserManagementService", url = "https://auth-backend-2-k3ph.onrender.com")
 public interface UserInterface {
 
     @GetMapping("/api/auth/userInfo/{aadharNumber}")
     ResponseEntity<ApiResponse<?>> getUserByAadharNumber(@PathVariable("aadharNumber") String aadharNumber);
-    @GetMapping("/api/auth/validate")
-    ResponseEntity<TokenResponseDto>  validateUser(@RequestHeader("token")  String token);
+//    @GetMapping("/api/auth/validate")
+//    ResponseEntity<TokenResponseDto>  validateUser(@RequestHeader("token")  String token);
 
 }
